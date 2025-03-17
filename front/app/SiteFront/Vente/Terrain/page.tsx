ca0d52d646b619fd20c7b4808e4f695ad27e8c90
Manambina09 <kellyandell9@gmail.com>
2025-03-17 16:01:51 +0300

    170

--- front/app/SiteFront/Vente/Terrain/page.tsx
+++ front/app/SiteFront/Vente/Terrain/page.tsx
@@ -107,7 +107,7 @@ export default function VenteTerrain() {
           <div className={styles.container}>
             <h1 className={terrainStyles.terrainHeroTitle}>Terrains à vendre</h1>
             <p className={terrainStyles.terrainHeroSubtitle}>
-              Construisez votre avenir sur l'un de nos terrains soigneusement sélectionnés
+              Construisez votre avenir sur l&apos;un de nos terrains soigneusement sélectionnés
             </p>
           </div>
         </div>
@@ -140,8 +140,8 @@ export default function VenteTerrain() {
                   <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M3.055 11H5a2 2 0 012 2v1a2 2 0 002 2 2 2 0 012 2v2.945M8 3.935V5.5A2.5 2.5 0 0010.5 8h.5a2 2 0 012 2 2 2 0 104 0 2 2 0 012-2h1.064M15 20.488V18a2 2 0 012-2h3.064M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
                 </svg>
               </div>
-              <h3>Choix de l'emplacement</h3>
-              <p>Sélectionnez l'environnement idéal pour votre futur lieu de vie.</p>
+              <h3>Choix de l&apos; emplacement</h3>
+              <p>Sélectionnez l&apos;environnement idéal pour votre futur lieu de vie.</p>
             </div>
           </div>
         </div>
